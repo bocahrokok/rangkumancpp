154d10f524ba36d27012f639cb8def89d77bfcaa
bocahrokok <indieunistudy@gmail.com>
2018-11-26 11:32:26 +0700

    Add pointer folder

--- rangkumanCpp/rangkumanAll.cpp
+++ rangkumanCpp/rangkumanAll.cpp
@@ -17,7 +17,7 @@ int main(){
     }
 git }
 
-//Mencari max and mind;
+//Mencari min and mind;
 
 int a, b, c;
 cin>> a;
@@ -25,30 +25,31 @@ cin>> b;
 cin>> c;
 
 if (a>b &&a > c){
-    cout << "The max : " << a<< endl;
+    cout << "The min : " << a<< endl;
 }
 else if ( b > a && b > c){
-    cout << "Max is :" << b << endl;
+    cout << "min is :" << b << endl;
 }
-else cout<< "Max is : "<< c;
+else {cout<< "min is : "<< c;}
 
-//different way to find max
-double max = myList[0];
+
+//different way to find min
+double min = myList[0];
 for (int i=1; i < ARRAY_SIZE; i++){
-    if (mylist[i]> max) max =myList[i]
+    if (mylist[i]> min) min =myList[i]
 } 
 //find min 
-double max = myList[0];
-int indexOfMax = 0;
+double min = myList[0];
+int indexOfmin = 0;
 for (int i=1; i < ARRAY_SIZE; i++){
-    if (myList[i] > max ){
-        max = myList[i];
-        indexOfMax = i;
+    if (myList[i] > min ){
+        min = myList[i];
+        indexOfmin = i;
 
     }
 }
 
-// find min and max AGAIN: 
+// find min and min AGAIN:
 
 int i, n ;
 float arr[100];
@@ -64,4 +65,113 @@ for (i = 1; i< n; i ){
     if (arr[0]< arr[i])
         arr[0] = arr[i];
 }
-cout <<"Max" << arr[0];
+cout <<"min" << arr[0];
+
+//finding average array;
+
+int n, i;
+
+float num[100], sum = 0.0, average;
+cin >> n;
+
+while (n > 100 || n<=0){
+    cout << "Angka antara 1-100";
+    cin >> n;
+}
+for (i=0; i<n; i++){
+    cout << i+1 << "enter NUM:";
+    cin >> num[i];
+    sum+= num[i];
+}
+average = sum/n;
+cout<< "Average" << average;
+
+//finding jumlah lulus atau enggak?
+cout << "how many?";
+cin >> n;
+
+lulus = 0;
+gagal = 0;
+
+for (a=1;a<=n; a++){
+    cout << "Insert mark student:";
+    cin >> b;
+    if (b>=50){
+        lulus = lulus+1;
+    }
+    else {
+        gagal = gagal+1;
+    }
+    cout << "sum lulus:"<< lulus<< endl;
+    cout << "sum gagal" << gagal << endl;}
+
+
+// sorting algorithm only;
+int temp, temp3, min;
+string temp2;
+
+for(int 0; i< 7-1; i++){
+    min=i; 
+    for (int j=i+1; j< 7; j++){
+        if (data[j].NISN < data[min].NISN){
+            min = j ;
+        }
+    }
+    temp = data[i].NISN;
+    temp2= data[i].name;
+    temp3= data[i].value;
+
+    data[i] = data[min].NISN;
+    data[i] = data[min].name;
+    data[i] = data[min].value;
+
+    data[min].NISN = temp;
+    data[min].name = temp2;
+    data[min].value = temp3;
+}
+
+//binary search algorithm
+
+int i = 0; j=6; k, query = 962;
+
+bool found= false;
+
+while (!found&&i<j){
+    k=(i+j)/2;
+    if(data[k].NISN< query){
+        i=k+1;
+    }
+    else if (data[k].NISN ==query){
+        found=true;
+    }
+    else {
+        j=k-1;
+    }
+    //print 
+    if(!found){
+        cout<<"Tidak ditemukan";
+    }else {
+        cout << "Ditemukan";
+    }
+}
+
+//sequential search;
+int query = 60;
+bool found = false;
+string temp4 = "Joko";
+
+//search algos;
+
+for (int i; i< 7; i++){
+    if (data[i].value == query){
+        data[i].name = temp4;
+        cout << data[i].NISN << " " data[i].name << " " << data[i].value << endl;
+        found = true;
+    }
+    if (!found){
+        cout << "Tidak Ditemukan";
+    }
+    
+}
+
+// 
